5a3ea7f2ab02f0d3e18a9621b262e7fcc55300d8
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-26 10:13:11 -0400

    test

---
+++ R/0PACKAGE.R
@@ -0,0 +1,19 @@
+
+
+#' 
+#' @details
+#' Help files of individual functions are intentionally suppressed in the `pdf` manual.
+#' Users are encouraged to get started with 
+#' 
+#' `vignette('intro', package = 'maxEff')`
+#' 
+#' 
+'_PACKAGE'
+
+
+
+
+
+
+
+
