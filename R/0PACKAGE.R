e1ac58a52835f43ea6506bfd8c9397982d73dee7
Tingting Zhan <tingtingzhan@gmail.com>
2026-03-16 10:08:16 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -1,6 +1,28 @@
 
 #' @keywords internal 
 #' 
+#' @import cli
+#' 
 #' @import stats
 #' 
 '_PACKAGE'
+
+
+#' @importFrom utils citation
+.onAttach <- function(libname, pkgname) {
+  
+  # .onAttach(libname = 'lib', pkgname = 'groupedHyperframe') # nah..
+  
+  'maxEff' |>
+    citation() |>
+    format(style = 'text') |> # utils:::format.citation
+    col_green() |>
+    style_bold() |>
+    cli_inform(class = 'packageStartupMessage')
+  
+  '\n' |>
+    cli_inform(class = 'packageStartupMessage')
+  
+}
+
+
